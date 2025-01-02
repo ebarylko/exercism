01ceff6dab6a29a2734924a53fb0e0e09e2b5bf3
Eitan Barylko <eitan@barylko.com>
2025-01-02 01:42:53 -0800

    Added files for this exercise

---
+++ haskell/leap/src/LeapYear.hs
@@ -0,0 +1,20 @@
+module LeapYear (isLeapYear) where
+
+somePred :: [a -> Bool] -> a -> Bool
+somePred [] _ = False
+somePred (f: funcs) x = f x || somePred funcs x
+
+everyPred :: [a -> Bool] -> a -> Bool
+everyPred [] _ = True
+everyPred (x: xs) el = x el && everyPred xs el
+
+reverseMod x y = mod y x
+divisibleBy400 :: Integer -> Bool
+divisibleBy400 x = ((== 0) . (reverseMod 400)) x
+
+divisibleBy4andNot100 :: Integer -> Bool
+divisibleBy4andNot100 x = everyPred [((/= 0) . (reverseMod 100)) ((== 0) . (reverseMod 4))] x
+
+isLeapYear :: Integer -> Bool
+-- isLeapYear year = somePred [(divisibleBy4andNot100) (divisibleBy400)] year
+isLeapYear year = (divisibleBy4andNot100 year) || (divisibleBy400 year)
