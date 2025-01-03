fe652edc42a6befc1be1cf7724d4e1272375f73b
Eitan Barylko <eitan@barylko.com>
2025-01-02 23:44:58 -0800

    Resolved the exercise

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,21 +1,15 @@
 module LeapYear (isLeapYear) where
-import Data.Predicate
+import Control.Applicative (liftA2)
 
-somePred :: [a -> Bool] -> a -> Bool
-somePred [] _ = False
-somePred (f: funcs) x = f x || somePred funcs x
+isZero = (== 0)
 
-everyPred :: [a -> Bool] -> a -> Bool
-everyPred [] _ = True
-everyPred (x: xs) el = x el && everyPred xs el
+modBy = flip mod 
+divisibleBy x y = isZero $ modBy x y
 
-reverseMod x y = mod y x
-divisibleBy400 :: Integer -> Bool
-divisibleBy400 x = ((== 0) . (reverseMod 400)) x
+(<&&>) = liftA2 (&&)
 
-divisibleBy4andNot100 :: Integer -> Bool
-divisibleBy4andNot100 =  [((/= 0) . (reverseMod 100)), ((== 0) . (reverseMod 4))] 
-
+(<||>) = liftA2 (||)
 isLeapYear :: Integer -> Bool
--- isLeapYear year = somePred [(divisibleBy4andNot100) (divisibleBy400)] year
-isLeapYear year = (divisibleBy4andNot100 year) || (divisibleBy400 year)
+isLeapYear =  divisibleBy4andNot100  <||>  (divisibleBy 400)
+  where
+    divisibleBy4andNot100 =  (not .  (divisibleBy 100))  <&&>  (divisibleBy 4)
