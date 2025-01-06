7fa87cb6392c55eb15c95b5169342d82a23ca66b
Eitan Barylko <eitan@barylko.com>
2025-01-05 20:28:54 -0800

    Resolved exercise

---
+++ ocaml/hamming/hamming.ml
@@ -0,0 +1,23 @@
+type nucleotide = A | C | G | T
+
+
+let same_length coll1 coll2 = List.length coll1 = List.length coll2
+
+let curr_distance acc (fst, snd) = (fst <> snd) |> Bool.to_int |> (+) acc
+
+let just x = Ok x
+
+let calc_distance list = List.fold_left curr_distance 0 list |> just
+
+let determine_error_msg coll1 coll2  = match (coll1, coll2) with
+  | [], _ -> "left strand must not be empty"
+  | _, [] -> "right strand must not be empty"
+  | _, _ ->  "left and right strands must be of equal length"
+
+let to_error msg = Error msg
+
+let error_msg coll1 coll2 = (determine_error_msg coll1 coll2) |> to_error 
+
+
+let hamming_distance coll1 coll2 = if same_length coll1 coll2 then calc_distance (List.combine coll1 coll2) else error_msg coll1 coll2
+  
