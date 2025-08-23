45f4d536ad2d4ce35badfd738cff4130baa152cb
Eitan Barylko <eitan@barylko.com>
2025-08-22 22:56:35 -0700

    Added files

---
+++ rust/gigasecond/src/lib.rs
@@ -0,0 +1,12 @@
+use time::PrimitiveDateTime as DateTime;
+use time::Duration;
+use std::ops::Add;
+
+fn gigasecond() -> Duration {
+    Duration::new(1_000_000_000, 0)
+}
+
+// Returns a DateTime one billion seconds after start.
+pub fn after(start: DateTime) -> DateTime {
+    start.add(gigasecond())
+}
--- rust/hello-world/src/lib.rs
+++ rust/hello-world/src/lib.rs
@@ -1,3 +1,5 @@
+use std::time;
+
 // &'static is a "lifetime specifier", something you'll learn more about later
 pub fn hello() -> &'static str {
     "Hello, World!"
