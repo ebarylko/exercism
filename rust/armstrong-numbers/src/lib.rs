d3e0682717a7a027b1820c30f525dccfc38b0401
Eitan Barylko <eitan@barylko.com>
2025-08-23 18:31:09 -0700

    Fixed an error

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,8 +1,12 @@
 use std::iter::successors;
 
+fn num_of_digits(num: u32) -> u32 {
+    num.to_string().len() as u32
+}
+
 pub fn is_armstrong_number(num: u32) -> bool {
     successors(Some(num), |&el| Some(el / 10)).
         take_while(|&el| el != 0).
-        map(|num: u32| (num % 10).pow(3))
+        map(|n: u32| (n % 10).pow(num_of_digits(num)))
         .sum::<u32>() == num
 }
