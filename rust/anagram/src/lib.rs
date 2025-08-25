4ec96a01f4eb5ccb4dfcd7312ac96d1d0a8c76ff
Eitan Barylko <62489101+ebarylko@users.noreply.github.com>
2025-08-25 16:59:38 -0700

    Anagram (#10)

---
+++ rust/anagram/src/lib.rs
@@ -0,0 +1,27 @@
+use std::collections::{HashSet, HashMap};
+
+type Freq = HashMap<char, u32>;
+
+fn update_count(c: char, letter_to_count: Freq) -> Freq {
+    let mut cp = HashMap::clone(&letter_to_count);
+    cp.entry(c).and_modify(|count| *count += 1).or_insert(1);
+    cp
+}
+
+pub fn frequencies(text: &str) -> Freq {
+    text
+        .chars()
+        .fold(HashMap::new(), |freq, c| update_count(c, freq))
+}
+
+fn is_anagram(target: &str, candidate: &str) -> bool {
+    frequencies(target) == frequencies(candidate) && target != candidate
+}
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    possible_anagrams.iter()
+        .filter(|src| is_anagram(&word.to_lowercase(), &src.to_lowercase()))
+        .copied()
+        .collect()
+
+}
