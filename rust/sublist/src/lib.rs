507196738e01b1087878781dd8b52391818405a0
Eitan Barylko <eitan@barylko.com>
2025-08-27 20:29:00 -0700

    Uncommented more tests

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 #[derive(Debug, PartialEq, Eq)]
 pub enum Comparison {
     Equal,
@@ -6,12 +7,34 @@ pub enum Comparison {
     Unequal,
 }
 
+/// Takes two lists, a, b, where a is smaller than b
+/// and returns Unequal if a is not a proper sublist of b.
+/// Returns Sublist otherwise
+fn is_proper_sublist_of(a: &[i32], b: &[i32]) -> Comparison {
+    Some(b)
+        .map(|coll| coll.windows(a.len()))
+        .filter(|coll| coll.clone().any(|sub_list| sub_list == a))
+        .map(|_| Comparison::Sublist)
+        .unwrap_or(Comparison::Unequal)
+}
+
+fn categorize_non_empty_lists(fst: &[i32], snd: &[i32]) -> Comparison {
+    match fst == snd {
+        true => Comparison::Equal,
+        _ => match fst.len().cmp(&snd.len()) {
+            Ordering::Equal => Comparison::Unequal,
+            Ordering::Less => is_proper_sublist_of(fst, snd),
+            _ => todo!("Need to implement this")
+        }
+    }
+}
+
 pub fn sublist(first_list: &[i32], second_list: &[i32]) -> Comparison {
     match (first_list, second_list) {
         ([], []) => Comparison::Equal,
         ([], _x) => Comparison::Sublist,
         (_x, []) => Comparison::Superlist,
-        (x, y) => Comparison::Equal
+        _ => categorize_non_empty_lists(first_list, second_list)
     }
 
 
